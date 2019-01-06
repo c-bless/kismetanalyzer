cfe4195961c69991a8f167f2c4b563545d7a755a
Christoph Bleß <bitbucket@cbless.de>
2019-01-06 02:19:05 +0100

    output example added to README

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='kismet-analyzer',
-    version='0.2.0',
+    version='0.2.1',
     author='Christoph Bless',
     author_email='bitbucket@cbless.de',
     url='https://bitbucket.org/cbless/kismetanalyzer',
