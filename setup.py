2d6432e1a89048743d47b30331726557169f3f41
Christoph Bless <bitbucket@cbless.de>
2020-01-22 22:56:34 +0100

    version info added in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='kismet-analyzer',
-    version='0.2.2',
+    version='0.3.0',
     author='Christoph Bless',
     author_email='bitbucket@cbless.de',
     url='https://bitbucket.org/cbless/kismetanalyzer',
